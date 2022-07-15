7ca1dda1f15565625a01202a9639feee03348bb4
Paul Thompson <pmt10@case.edu>
2022-07-15 12:37:32 -0400

    hourglass module added

--- include/hourglass.hpp
+++ include/hourglass.hpp
@@ -39,3 +39,109 @@ struct FirstLayerImpl : nn::Module {
     Residual r2;
 };
 TORCH_MODULE(FirstLayer);
+
+struct HourglassImpl : nn::Module {
+    HourglassImpl(int N)
+        : branch_res1(N,N),
+        p1(nn::MaxPool2dOptions({2,2})),
+        res1(N,N),
+
+        branch_res2(N,N),
+        p2(nn::MaxPool2dOptions({2,2})),
+        res2(N,N),
+
+        branch_res3(N,N),
+        p3(nn::MaxPool2dOptions({2,2})),
+        res3(N,N),
+
+        branch_res4(N,N),
+        p4(nn::MaxPool2dOptions({2,2})),
+        res4(N,N),
+
+        res5(N,N),
+
+        res6(N,N),
+        unpool4(nn::MaxUnpool2dOptions({2,2})),
+
+        res7(N,N),
+        unpool3(nn::MaxUnpool2dOptions({2,2})),
+
+        res8(N,N),
+        unpool2(nn::MaxUnpool2dOptions({2,2})),
+
+        res9(N,N),
+        unpool1(nn::MaxUnpool2dOptions({2,2}))
+        
+ {
+   register_module("branch_res1",branch_res1);
+   register_module("p1",p1); // 128 x 128 -> 64 x 64
+   register_module("res1",res1);
+
+   register_module("branch_res2",branch_res2);
+   register_module("p2",p2); // 64 x 64 -> 32 x 32
+   register_module("res2",res2);
+
+   register_module("branch_res3",branch_res3);
+   register_module("p3",p3); // 32 x 32 -> 16 x 16
+   register_module("res3",res3);
+
+   register_module("branch_res4",branch_res4);
+   register_module("p4",p4); // 16 x 16 -> 8 x 8
+   register_module("res4",res4);
+
+   register_module("res5",res5);
+
+   register_module("res6",res6);
+   register_module("unpool4",unpool4); // 8 x 8 -> 16 x 16
+
+   register_module("res7",res7);
+   register_module("unpool3",unpool3); // 16 x 16 -> 32 x 32
+
+   register_module("res8",res8);
+   register_module("unpool2",unpool2); // 32 x 32 -> 64 x 64
+
+   register_module("res9",res9);
+   register_module("unpool1",unpool1); // 64 x 64 -> 128 x 128
+
+ }
+
+  torch::Tensor forward(torch::Tensor input) {
+
+   torch::Tensor up1 = branch_res1(input);
+   auto [out1, indices1]= p1->forward_with_indices(input);
+   out1 = res1(out1);
+
+   torch::Tensor up2 = branch_res2(out1);
+   auto  [out2,indices2] = p2->forward_with_indices(out1);
+   out2 = res2(out2);
+
+   torch::Tensor up3 = branch_res3(out2);
+   auto [out3, indices3] = p3->forward_with_indices(out2);
+   out3 = res3(out3);
+
+   torch::Tensor up4 = branch_res4(out3);
+   auto [out4, indices4] = p4->forward_with_indices(out3);
+   out4 = res4(out4);
+
+   out4 = res5(out4);
+
+   out4 = res6(out4);
+   torch::Tensor x = unpool4(out4,indices4) + up4;
+
+   x = res7(x);
+   x = unpool3(x,indices3) + up3;
+  
+   x = res8(x);
+   x = unpool2(x,indices2) + up2;
+
+   x = res9(x);
+   x = unpool1(x,indices1) + up1;
+
+   return x;
+ }
+    Residual branch_res1, branch_res2, branch_res3, branch_res4;
+    Residual res1, res2, res3, res4, res5, res6,res7, res8, res9;
+    nn::MaxPool2d p1, p2, p3, p4;
+    nn::MaxUnpool2d unpool4,unpool3,unpool2,unpool1;
+};
+TORCH_MODULE(Hourglass);
