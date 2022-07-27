7aec5bb6ddbacdefc1366dadfc4f72e74f1c3664
Paul Thompson <pmt10@case.edu>
2022-07-27 11:41:15 -0400

    can receive options in commandline and start to parse json file

--- hourglass.cpp
+++ hourglass.cpp
@@ -2,9 +2,39 @@
 #include "include/hourglass.hpp"
 #include "include/dataload.hpp"
 
+#include <cxxopts.hpp>
+
 #include <iostream>
 
-int main() {
+int main(int argc, char** argv) {
+
+  cxxopts::Options options("Hourglass Neural Network", "Commandline interface for hourglass neural network");
+
+  options.add_options()
+    ("h,help", "Print usage")
+    ("d,data", "Data JSON configuration file", cxxopts::value<std::string>())
+  ;
+
+  auto result = options.parse(argc, argv);
+
+  if (result.count("help"))
+  {
+    std::cout << options.help() << std::endl;
+    exit(0);
+  } else if (result.count("data")) {
+    std::cout << "entering data method" << std::endl;
+
+    std::cout << "Configuration file is " << result["data"].as<std::string>() << std::endl;
+
+    read_json_file(result["data"].as<std::string>());
+
+    exit(0);
+  }
+
+
   torch::Tensor tensor = torch::rand({2, 3});
   std::cout << tensor << std::endl;
+
+
+  return 0;
 }
--- include/dataload.hpp
+++ include/dataload.hpp
@@ -1,5 +1,53 @@
  #include <torch/torch.h>
  #include <nlohmann/json.hpp>
 
+ #include <iostream>
+
  using namespace torch;
  using json = nlohmann::json;
+
+
+void read_json_file(const std::string& config_file) {
+
+    std::ifstream f(config_file);
+    json data = json::parse(f);
+    f.close();
+
+    std::cout << data["name"] << std::endl;
+};
+
+
+ //https://discuss.pytorch.org/t/libtorch-how-to-use-torch-datasets-for-custom-dataset/34221/2
+ torch::Tensor read_img(const std::string& config_file) 
+ {
+    torch::Tensor tensor;
+    return tensor;
+ };
+
+ torch::Tensor read_labels(const std::string& config_file)
+ {
+    torch::Tensor tensor;
+    return tensor;
+ };
+
+ class MyDataset : public torch::data::Dataset<MyDataset>
+{
+    private:
+        torch::Tensor states_, labels_;
+
+    public:
+        explicit MyDataset(const std::string& config_file) 
+            : states_(read_img(config_file)),
+              labels_(read_labels(config_file))
+        {
+
+        };
+        torch::data::Example<> get(size_t index) override;
+};
+
+torch::data::Example<> MyDataset::get(size_t index)
+{
+    // You may for example also read in a .csv file that stores locations
+    // to your data and then read in the data at this step. Be creative.
+    return {states_[index], labels_[index]};
+} 
