3905c68a0ff7cd9b1574295c6f6f028856919c4d
Paul <pmt10@case.edu>
2022-08-14 16:38:58 -0400

    can now take beginning and end of frames

--- include/prediction.hpp
+++ include/prediction.hpp
@@ -2,6 +2,7 @@
 #include <nlohmann/json.hpp>
 #include <opencv2/opencv.hpp>
 #include <ffmpeg_wrapper/videodecoder.h>
+#include <ffmpeg_wrapper/videoencoder.h>
 
 #include "hourglass.hpp"
 #include "dataload.hpp"
@@ -45,7 +46,7 @@ cv::Mat combine_overlay(cv::Mat& img, cv::Mat& label) {
 
     cv::split(color_img,channel);
 
-    cv::addWeighted(channel[1],0.5, label,0.5,0.0,channel[1]);
+    cv::addWeighted(channel[0],0.5, label,0.5,0.0,channel[0]);
 
     /*
     for (int i=0; i< color_img.rows; i++) {
@@ -141,10 +142,20 @@ void predict_video(StackedHourglass &hourglass, torch::Device device, const std:
     std::cout << data["prediction"]["videos"] << std::endl;
 
     auto vd = ffmpeg_wrapper::VideoDecoder();
+    //auto ve = ffmpeg_wrapper::VideoEncoder();
      
     std::string vid_name = data["prediction"]["videos"];
     vd.createMedia(vid_name);
-    const int64_t total_images = vd.getFrameCount();
+    int64_t total_images = vd.getFrameCount();
+    int64_t frame_index = 0;
+
+    if (data["prediction"].contains("start_frame")) {
+        frame_index = data["prediction"]["start_frame"];
+    }
+
+    if (data["prediction"].contains("end_frame")) {
+        total_images = data["prediction"]["end_frame"];
+    }
 
     std::cout << "Video loaded with " << total_images << " frames" << std::endl;
     int batch_size = data["prediction"]["batch-size"];
@@ -157,10 +168,10 @@ void predict_video(StackedHourglass &hourglass, torch::Device device, const std:
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    std::vector<uint8_t> image = vd.getFrame(0, true);
+    std::vector<uint8_t> image = vd.getFrame(0);
 
     int64_t batch_index = 0;
-    int64_t frame_index = 0;
+    
     while (frame_index < total_images)
     {
         int last_index = frame_index + batch_size - 1;
@@ -180,6 +191,7 @@ void predict_video(StackedHourglass &hourglass, torch::Device device, const std:
 
         auto data_raw_data_ptr = data.data_ptr<uchar>();
         
+        /*
         for (int j = 0; j < prediction.size(3); j++) {
 
             cv::Mat resultImg(out_height,out_width,CV_8UC1, prediction_raw_data_ptr + (out_height*out_width*j));
@@ -190,6 +202,7 @@ void predict_video(StackedHourglass &hourglass, torch::Device device, const std:
             std::string img_name = "test" + std::to_string(batch_index) + "-" + std::to_string(j) + ".png";
             cv::imwrite(img_name,resultImg);
         }
+        */
         
         std::cout << "\r"
                     "["
